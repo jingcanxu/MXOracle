6701f97d879e30d10eaaad68ce9ffbfee962d0cf
Jingcan Xu <Jingcan Xu@LAPTOP-S4UKOPQ5>
2020-02-02 18:10:35 +0800

    free tr

---
+++ Generated-20200202180533.sql
@@ -0,0 +1,10 @@
+--------------------------------------------------------
+--  File created - Sunday-February-02-2020   
+--------------------------------------------------------
+@C:\Users\Jingcan Xu\Oracle Code\MX\MXOracle\Z_MY_TEST.sql
+@C:\Users\Jingcan Xu\Oracle Code\MX\MXOracle\Z_SRC_KIT_DETAIL.sql
+@C:\Users\Jingcan Xu\Oracle Code\MX\MXOracle\Z_SRC_KIT_KITS.sql
+@C:\Users\Jingcan Xu\Oracle Code\MX\MXOracle\Z_SRC_USER.sql
+@C:\Users\Jingcan Xu\Oracle Code\MX\MXOracle\PK_Z_SRC_USER.sql
+@C:\Users\Jingcan Xu\Oracle Code\MX\MXOracle\TRG_TEST.sql
+@C:\Users\Jingcan Xu\Oracle Code\MX\MXOracle\Z_SRC_USER_CONSTRAINT.sql
---
+++ TRG_TEST.sql
@@ -0,0 +1,12 @@
+--------------------------------------------------------
+--  DDL for Trigger TRG_TEST
+--------------------------------------------------------
+
+  CREATE OR REPLACE TRIGGER "MX"."TRG_TEST" 
+before insert on Z_SRC_USER
+for each row
+begin
+select user_seq.nextval into :new.id from dual;--into?new??????into???????new???????????--
+end;
+/
+ALTER TRIGGER "MX"."TRG_TEST" ENABLE;
